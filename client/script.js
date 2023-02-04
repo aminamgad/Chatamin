432c944c28a88e8353070183c36f2d7055717ffc
Amin Amgad <96820371+aminamgad@users.noreply.github.com>
2023-02-04 17:33:30 +0200

    up

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatamin.onrender.com/cl', {
+    const response = await fetch('https://chatamin.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -48,5 +48,5 @@ app.post('/', async (req, res) => {
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
 
 
-///hide
+/// https://chatamin.onrender.com/
    
